646def50ce9e11ed850c204e250ae24f46f389fb
Riyaz <ahmed.riyaz94@gmail.com>
2019-11-15 17:15:17 +0530

    created page not found route

--- src/modules/routes.js
+++ src/modules/routes.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Switch, Route, Redirect, HashRouter } from 'react-router-dom';
 import HomePage from './homePage';
 import ProductDetailsPage from './productDetailsPage';
+import PageNotFound from './commonComponents/pageNotFound';
 
 const App = () => {
   return (
     <Switch>
         <Route exact path="/" component={HomePage}/>
         <Route exact path="/product-details/:slug?/:productId?" component={ProductDetailsPage} />
+        <Route component={PageNotFound} />
     </Switch>
   );
 }
