f9e618d08b7049b04871a98d5faa811a0de0072f
Riyaz <ahmed.riyaz94@gmail.com>
2019-11-17 01:50:39 +0530

    included app icon

--- src/modules/commonContainers/sectionedContainer/index.js
+++ src/modules/commonContainers/sectionedContainer/index.js
@@ -3,17 +3,18 @@ import SectionedHeader from 'CommonContainers/sectionedHeader';
 import DivRow from 'CommonComponents/divRow';
 import DivColumn from 'CommonComponents/divColumn';
 import styles from './sectioned_container.module.scss';
+import appIcon from 'Icons/app-icon-white.svg'
 
 export default class SectionedContainer extends Component {
 
   render() {
      return(
        <DivRow className={styles.page_container}>
-         <div className={styles.left_container}>
-            <div className={styles.header_container}>
-              
-            </div>
-         </div>
+         <DivColumn className={styles.left_container}>
+            <DivRow className={styles.header_container}>
+              <img src={appIcon} className={styles.app_icon}/>
+            </DivRow>
+         </DivColumn>
 
           <DivColumn className={styles.right_container}>
             <SectionedHeader />
--- src/modules/homePage/index.js
+++ src/modules/homePage/index.js
@@ -6,7 +6,7 @@ export default class HomePage extends Component {
   render() {
      return (
        <SectionedContainer>
-
+         
        </SectionedContainer>
      )
   }
