48127112dcd28e8ad426cdb865d3016b79c33388
Riyaz <ahmed.riyaz94@gmail.com>
2019-11-17 15:35:18 +0530

    added social icons for homepage

--- src/modules/homePage/index.js
+++ src/modules/homePage/index.js
@@ -4,6 +4,9 @@ import SectionedContainer from 'CommonContainers/sectionedContainer';
 import DivColumn from 'CommonComponents/divColumn';
 import DivRow from 'CommonComponents/divRow';
 import ExhibitionItemContainer from './exhibitionItemContainer';
+import socialFacebookIcon from 'Icons/social-facebook-icon-white.svg';
+import socialInstagramIcon from 'Icons/social-instagram-icon-white.svg';
+import socialTwitterIcon from 'Icons/social-twitter-icon-white.svg';
 
 export default class HomePage extends Component {
   render() {
@@ -12,9 +15,9 @@ export default class HomePage extends Component {
          <DivColumn className={styles.home_container}>
            <DivRow className={styles.content_container}>
              <DivColumn className={styles.social_container}>
-                <div>facebook</div>
-                <div>twitter</div>
-                <div>instagram</div>
+                <div className={styles.social_item_container}><img src={socialFacebookIcon} className={styles.social_image}/></div>
+                <div className={styles.social_item_container}><img src={socialTwitterIcon} className={styles.social_image}/></div>
+                <div className={styles.social_item_container}><img src={socialInstagramIcon} className={styles.social_image}/></div>
              </DivColumn>
              <ExhibitionItemContainer />
            </DivRow>
