b6770edf37d3154fb13da722ea32005d721e9dfc
Riyaz <ahmed.riyaz94@gmail.com>
2019-11-17 19:07:15 +0530

    removed build from git ignore

---
+++ build/precache-manifest.6d7894ef7e282afe663a62debaf6d7b9.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc1161a63a0421107fd3b311998ef659",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9ae6a087f79735d403d1",
+    "url": "/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "d220965dc075da83f2af",
+    "url": "/static/css/main.2f0b93a5.chunk.css"
+  },
+  {
+    "revision": "9ae6a087f79735d403d1",
+    "url": "/static/js/2.4ae95e82.chunk.js"
+  },
+  {
+    "revision": "d220965dc075da83f2af",
+    "url": "/static/js/main.eeb5dd3e.chunk.js"
+  },
+  {
+    "revision": "73de5f499f1f7ece7aa9",
+    "url": "/static/js/runtime-main.afa85a71.js"
+  },
+  {
+    "revision": "8844958f9db0befbbe668f3812ffafaa",
+    "url": "/static/media/app-icon-white.8844958f.svg"
+  },
+  {
+    "revision": "792a5537dad44c53759a0aeb7275568c",
+    "url": "/static/media/arrow-left-icon-black.792a5537.svg"
+  },
+  {
+    "revision": "97adcc4a98f57f8ed07c79bc289716a4",
+    "url": "/static/media/arrow-right-icon-black.97adcc4a.svg"
+  },
+  {
+    "revision": "bcbb085c68ab1645292820112f9501f7",
+    "url": "/static/media/exhibition-item-1.bcbb085c.jpg"
+  },
+  {
+    "revision": "5b9b618b08cc7345878ca500314e06ec",
+    "url": "/static/media/hamburger-menu-icon-black.5b9b618b.svg"
+  },
+  {
+    "revision": "d745a67458fb4ac928d0998c14f04379",
+    "url": "/static/media/scroll-down-icon-white.d745a674.svg"
+  },
+  {
+    "revision": "85ce852e5bd0543303c416fa7eb66642",
+    "url": "/static/media/search-icon-black.85ce852e.svg"
+  },
+  {
+    "revision": "40c3f9a6d3c231b76bf65205314f1a5d",
+    "url": "/static/media/share-icon-black.40c3f9a6.svg"
+  },
+  {
+    "revision": "76128716a23c8955810be7086479413f",
+    "url": "/static/media/social-facebook-icon-white.76128716.svg"
+  },
+  {
+    "revision": "3d3ff02882fe0187decaa2d30599d0ab",
+    "url": "/static/media/social-instagram-icon-white.3d3ff028.svg"
+  },
+  {
+    "revision": "62f9af12279b101be824a38543e3fd7d",
+    "url": "/static/media/social-twitter-icon-white.62f9af12.svg"
+  }
+]);
