572162ed87c0f1e40a7501fea7c2adbd57514739
Riyaz <ahmed.riyaz94@gmail.com>
2019-11-18 19:57:28 +0530

    minor changes refelected for header

--- src/modules/commonComponents/divRow/index.js
+++ src/modules/commonComponents/divRow/index.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
 import styles from './div_row.module.scss';
 
-const DivRow = ({className='', children}) => {
+const DivRow = ({className='', verticalCenter, horizontalCenter ,children}) => {
   return (
-    <div className={`${styles.flex_row} ${className}`}>
+    <div 
+     className={`
+      ${styles.flex_row} ${className}
+      ${verticalCenter ? styles.align_center : ''}
+      ${horizontalCenter ? styles.justify_center: ''}`}
+    >
       {children}
     </div>
   )
