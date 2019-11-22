a91835d93e26c2f3660433cbe80f72d536d7873b
Riyaz <ahmed.riyaz94@gmail.com>
2019-11-22 13:59:19 +0530

    design pdp page left container

--- src/modules/pages/productDetailsPage/index.js
+++ src/modules/pages/productDetailsPage/index.js
@@ -3,21 +3,29 @@ import FullWidthContainer from 'CommonContainers/fullwidthContainer';
 import DivRow from 'CommonComponents/divRow';
 import DivColumn from 'CommonComponents/divColumn';
 import styles from './product_details_page.module.scss';
+import exhibitionImage1 from 'Images/exhibition-item-1.jpg';
+import exhibitionImage2 from 'Images/exhibition-item-2.jpg';
+import exhibitionImage3 from 'Images/exhibition-item-3.png';
 
 export default class ProductDetailsPage extends Component {
-
   render() {
     return (
       <FullWidthContainer>
         <DivColumn className={styles.product_details_container}>
-         <div>back button</div>
+         <div>Back Button</div>
          <DivRow className={styles.product_content_container}>
-           <DivColumn className={styles.left_content_container}>
-             product image
-           </DivColumn>
-           <DivColumn className={styles.right_content_container}>
 
+           <DivColumn className={styles.left_content_container}>
+            <img src={exhibitionImage3} className={styles.product_image} />
+            <DivRow className={styles.product_image_list}>
+              <img src={exhibitionImage1} className={styles.small_product_image} />
+              <img src={exhibitionImage2} className={styles.small_product_image} />
+            </DivRow>
            </DivColumn>
+
+           <DivColumn className={styles.right_content_container}>
+           </DivColumn>
+
          </DivRow>
         </DivColumn>
       </FullWidthContainer>
