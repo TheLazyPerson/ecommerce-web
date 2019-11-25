8698538d466b054b14c69edd169cd4c3422e7c5e
Riyaz Ahmed <ahmed.riyaz94@gmail.com>
2019-11-25 21:21:34 +0530

    created seperate component for exhibition item component

--- src/modules/pages/searchPage/index.js
+++ src/modules/pages/searchPage/index.js
@@ -3,9 +3,7 @@ import FullWidthContainer from 'CommonContainers/fullwidthContainer';
 import DivRow from 'CommonComponents/divRow';
 import DivColumn from 'CommonComponents/divColumn';
 import styles from './search_page.module.scss';
-import CapsuleButton from 'CommonComponents/capsuleButton';
-import CapsuleText from 'CommonComponents/capsuleText';
-import exhibitionImage1 from 'Images/exhibition-item-1.jpg';
+import ExhibitionItemComponent from './exhibitionItemComponent';
 
 export default class SearchPage extends Component {
   render() {
@@ -21,26 +19,7 @@ export default class SearchPage extends Component {
             
             <DivRow>
 
-              <DivRow className={styles.exhibition_item_container}>
-                <img src={exhibitionImage1} className={styles.exhibition_image}/>
-                
-                <DivColumn className={styles.exhibition_details_container}>
-                  <div className={styles.exhibition_name}>
-                    The Craft Show
-                  </div>
-                  <div className={styles.exhibition_details}>
-                    The Craft Show will display products like Handcrafted Watches, Products, Farsis, Palazzos,  Culottes and Products.With love, and much more.
-                  </div>
-
-                  <DivRow className={styles.capsule_container}>
-                    <CapsuleText text="watches" className={styles.capsule}/>
-                    <CapsuleText text="craft" className={styles.capsule}/>
-                    <CapsuleText text="crafted" className={styles.capsule}/>
-                  </DivRow>
-
-                  <CapsuleButton className={styles.view_exhibition_button}>View Exhibition</CapsuleButton>
-                </DivColumn>
-              </DivRow>
+              <ExhibitionItemComponent />
 
             </DivRow>
           </DivColumn>
