f58a11c60608518d1de8fe0566838ff050614a81
Riyaz Ahmed <ahmed.riyaz94@gmail.com>
2019-11-26 22:41:43 +0530

    completed designs for checkout

--- src/modules/commonComponents/horizontalBorder/index.js
+++ src/modules/commonComponents/horizontalBorder/index.js
@@ -3,8 +3,10 @@ import styles from './horizontal_border.module.scss';
 
 export default class HorizontalBorder extends Component {
   render() {
+    const { className } = this.props;
+
      return(
-       <div className={styles.horizontal_border_div}></div>
+       <div className={`${styles.horizontal_border_div} ${className}`} />
      )
   }
 }
--- src/modules/pages/checkoutPage/index.js
+++ src/modules/pages/checkoutPage/index.js
@@ -6,6 +6,7 @@ import styles from './checkout_page.module.scss';
 import CheckoutItemComponent from './checkoutItemComponent';
 import HorizontalBorder from 'CommonComponents/horizontalBorder';
 import couponIcon from 'Icons/coupon-icon-white.svg';
+import CapsuleButton from 'CommonComponents/capsuleButton';
 
 export default class CheckoutPage extends Component {
   render() {
@@ -35,59 +36,66 @@ export default class CheckoutPage extends Component {
 
           </DivColumn>
           
-          <DivColumn className={styles.order_summary_container}>
-            <div className={styles.order_summary_title}>Order Summary</div>
-            <HorizontalBorder />
-            <DivRow verticalCenter className={styles.coupon_input}>
-              <img src={couponIcon} className={styles.icon} />
-              <input type="text" placeholder="Apply Coupon" className={styles.input} />
-              <div className={styles.apply_button}>APPLY</div>
-            </DivRow>
-            <HorizontalBorder />
+          <DivColumn>
+            <DivColumn className={styles.order_summary_container}>
+              <div className={styles.order_summary_title}>Order Summary</div>
+              <HorizontalBorder />
+              <DivRow verticalCenter className={styles.coupon_input}>
+                <img src={couponIcon} className={styles.icon} />
+                <input type="text" placeholder="Apply Coupon" className={styles.input} />
+                <div className={styles.apply_button}>APPLY</div>
+              </DivRow>
+              <HorizontalBorder />
 
-            <div className={styles.coupon_header_text}>Coupons</div>
-            <DivColumn className={styles.coupon_description_container}>
-              <DivColumn className={styles.coupon_content_container}>
-                <div className={styles.coupon_title}>40% OFF up to KD 29</div>
-                <div className={styles.coupon_description}>
-                  On order of KD 400  and above. Valid once per user.
-                </div>
+              <div className={styles.coupon_header_text}>Coupons</div>
+              <DivColumn className={styles.coupon_description_container}>
+                <DivColumn className={styles.coupon_content_container}>
+                  <div className={styles.coupon_title}>40% OFF up to KD 29</div>
+                  <div className={styles.coupon_description}>
+                    On order of KD 400  and above. Valid once per user.
+                  </div>
+                </DivColumn>
+                <HorizontalBorder />
+                <DivRow className={styles.coupon_container}>
+                  <div className={styles.coupon}>FREEITEM29</div>
+                  <div className={styles.coupon_apply}>APPLY</div>
+                </DivRow>
               </DivColumn>
+              
               <HorizontalBorder />
-              <DivRow className={styles.coupon_container}>
-                <div className={styles.coupon}>FREEITEM29</div>
-                <div className={styles.coupon_apply}>APPLY</div>
-              </DivRow>
+
+              <DivColumn>
+                <div className={styles.coupon_header_text}>Price Details</div>
+                <DivRow className={styles.price_details_container}>
+                  <div className={styles.title}>Bag Total</div>
+                  <div className={styles.value}>KD 299</div>
+                </DivRow>
+                <DivRow className={styles.price_details_container}>
+                  <div className={styles.title}>Coupon Discount</div>
+                  <div className={styles.value}>Apply  Coupon</div>
+                </DivRow>
+                <DivRow className={styles.price_details_container}>
+                  <div className={styles.title}>Order Total</div>
+                  <div className={styles.value}>KD 299</div>
+                </DivRow>
+                <DivRow className={styles.price_details_container}>
+                  <div className={styles.title}>Delivery Charges</div>
+                  <div className={styles.value}>FREE</div>
+                </DivRow>
+
+                <HorizontalBorder className={styles.price_divider}/>
+
+                <DivRow className={styles.price_details_container}>
+                  <div className={styles.title}>Total</div>
+                  <div className={styles.value}>KD 299</div>
+                </DivRow>
+              </DivColumn>           
+
+              <CapsuleButton className={styles.capsule_button}>
+                Place Order
+              </CapsuleButton>
+              
             </DivColumn>
-            
-            <HorizontalBorder />
-
-            <DivColumn>
-              <div className={styles.coupon_header_text}>Price Details</div>
-              <DivRow>
-                <div>Bag Total</div>
-                <div>KD 299</div>
-              </DivRow>
-              <DivRow>
-                <div>Coupon Discount</div>
-                <div>Apply  Coupon</div>
-              </DivRow>
-              <DivRow>
-                <div>Order Total</div>
-                <div>KD 299</div>
-              </DivRow>
-              <DivRow>
-                <div>Delivery Charges</div>
-                <div>FREE</div>
-              </DivRow>
-              <HorizontalBorder />
-              <DivRow>
-                <div>Total</div>
-                <div>KD 299</div>
-              </DivRow>
-            </DivColumn>            
-
-            
           </DivColumn>
         </DivRow>
       </FullWidthContainer>
