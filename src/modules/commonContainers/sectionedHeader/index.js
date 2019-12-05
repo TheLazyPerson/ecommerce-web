b7546744bad378d8fe9e04199d2d6591333fd19f
Riyaz <ahmed.riyaz94@gmail.com>
2019-12-05 19:56:40 +0530

    disabled autcomplete for search

--- src/modules/commonContainers/sectionedHeader/index.js
+++ src/modules/commonContainers/sectionedHeader/index.js
@@ -104,7 +104,7 @@ class SectionedHeader extends Component {
                 onChange={this.onChangeSearchText}
                 onFocus={this.showSearchBar}
                 onBlur={this.hideSearchBar}
-                autoComplete="autocomplete_off_hack_xfr4!k"
+                autoComplete="off"
               />
             </form>
             <img src={searchIcon} className={styles.search_icon}/>
