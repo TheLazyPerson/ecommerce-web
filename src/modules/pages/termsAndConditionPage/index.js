21b21be7dfb9a128d5140fe912e73df3c1b94f38
Riyaz Ahmed <ahmed.riyaz94@gmail.com>
2019-12-08 00:33:59 +0530

    created logic for terms and condition text layout

--- src/modules/pages/termsAndConditionPage/index.js
+++ src/modules/pages/termsAndConditionPage/index.js
@@ -3,10 +3,25 @@ import SectionedContainer from "CommonContainers/sectionedContainer";
 import DivColumn from 'CommonComponents/divColumn';
 import StaticPageHeader from 'CommonComponents/staticPageHeader';
 import styles from './terms_and_condition.module.scss';
+import map from 'lodash/map';
 
 export default class TermsAndConditionPage extends Component {
   constructor(props) {
     super(props);
+  }
+
+  renderDescription = (description) => {
+    return map(description, descriptionItem => {
+      if (descriptionItem.type == 'normal') {
+        return (
+          <div>{description.value}</div>
+        )
+      } else if (descriptionItem.type == 'points') {
+        return map(descriptionItem.value, (descriptionPoint, index) => {
+          return <div>{`${index+1}. ${descriptionPoint}`}</div>
+        })
+      }
+    })
   }
 
   render() {
@@ -14,7 +29,28 @@ export default class TermsAndConditionPage extends Component {
        <SectionedContainer 
         sideBarContainer={<StaticPageHeader subTitle="TERMS OF USE" title="Terms and Conditions"/>}
        >
+        <DivColumn fillParent className={styles.page_container}>
+          {
+            map([], item => {
 
+              switch(item.type) {
+                case 'last-updated':
+                  return (
+                    <div>{item.value}</div>
+                  )
+                case 'q-n-a':
+                  return (
+                    <DivColumn fillSelfHorizontal>
+                      <div>{item.value.title}</div>
+
+                      <div>{this.renderDescription(item.value.description)}</div>
+                    </DivColumn>
+                  )
+              }
+
+            })
+          }
+        </DivColumn>
        </SectionedContainer>
      )
   }
