4f46fb64361f4e93e8841cda7f4b4d777b47ca64
Kunal Pardeshi <kunalpardeshi280@gmail.com>
2019-12-08 01:25:46 +0530

    updated profile address and detail

--- src/modules/pages/profilePages/profileDetails/index.js
+++ src/modules/pages/profilePages/profileDetails/index.js
@@ -1,76 +1,99 @@
-import React, { Component } from 'react';
-import SectionedContainer from 'CommonContainers/sectionedContainer';
-import DivColumn from 'CommonComponents/divColumn';
-import DivRow from 'CommonComponents/divRow';
-import SideNav from '../components/sideNav';
-import styles from './profile_details.module.scss';
-import NavHeader from '../components/navHeader';
-import map from 'lodash/map';
-import CapsuleButton from 'CommonComponents/capsuleButton';
-import SecondaryCapsuleButton from 'CommonComponents/secondaryCapsuleButton';
+import React, { Component } from "react";
+import SectionedContainer from "CommonContainers/sectionedContainer";
+import DivColumn from "CommonComponents/divColumn";
+import DivRow from "CommonComponents/divRow";
+import SideNav from "../components/sideNav";
+import styles from "./profile_details.module.scss";
+import NavHeader from "../components/navHeader";
+import map from "lodash/map";
+import CapsuleButton from "CommonComponents/capsuleButton";
+import SecondaryCapsuleButton from "CommonComponents/secondaryCapsuleButton";
+import { getProfileDetailsAction } from "Core/modules/profiledetails/profileDetailsActions";
+import InitialPageLoader from "CommonContainers/initialPageLoader";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
 
-export default class ProfileDetails extends Component {
+class ProfileDetails extends Component {
   state = {
     userInfo: [
       {
-        title: 'First Name',
-        value: 'Omar'  
+        title: "First Name",
+        value: "Omar"
       },
       {
-        title: 'Last Name',
-        value: 'Lastname'  
+        title: "Last Name",
+        value: "Lastname"
       },
       {
-        title: 'Email Address',
-        value: 'omarlastname@mail.com'
+        title: "Email Address",
+        value: "omarlastname@mail.com"
       },
       {
-        title: 'Phone Number',
-        value: '+965-955-5836-852'
+        title: "Phone Number",
+        value: "+965-955-5836-852"
       },
       {
-        title: 'Gender',
-        value: 'NOTSET'
+        title: "Gender",
+        value: "NOTSET"
       },
       {
-        title: 'Birthday',
-        value: 'NOTSET'
+        title: "Birthday",
+        value: "NOTSET"
       }
     ]
-  }
+  };
   render() {
-    const {userInfo} = this.state;
+    const { userInfo } = this.state;
+    const {
+      profileDetailsReducer: { userDetails },
+      getProfileDetailsAction
+    } = this.props;
 
-     return (
-      <SectionedContainer
-        sideBarContainer={<SideNav />}
-      >
+    const user = userDetails.user_details;
+    console.log(userDetails);
+
+    return (
+      <SectionedContainer sideBarContainer={<SideNav />}>
         <DivColumn className={styles.details_container}>
-        <NavHeader title="profile details">
-          <DivRow>
-          <SecondaryCapsuleButton className={styles.reset_password_button}>
-            Change Password
-          </SecondaryCapsuleButton>
+          <NavHeader title="profile details">
+            <DivRow>
+              <SecondaryCapsuleButton className={styles.reset_password_button}>
+                Change Password
+              </SecondaryCapsuleButton>
 
-          <CapsuleButton>
-            Edit Profile
-          </CapsuleButton>
-          </DivRow>
-        </NavHeader>
-        
-        <DivColumn fillParent>
-         {
-           map(userInfo, user => (
-             <DivColumn className={styles.field_container}>
-              <div className={styles.title}>{user.title}</div>
-              <div className={styles.value}>{user.value}</div>
-             </DivColumn>
-           ))
-         }
-        </DivColumn>
+              <CapsuleButton>Edit Profile</CapsuleButton>
+            </DivRow>
+          </NavHeader>
 
+          <InitialPageLoader initialPageApi={getProfileDetailsAction}>
+            <DivColumn fillParent>
+              {map(userInfo, user => (
+                <DivColumn className={styles.field_container}>
+                  <div className={styles.title}>{user.title}</div>
+                  <div className={styles.value}>{user.value}</div>
+                </DivColumn>
+              ))}
+            </DivColumn>
+          </InitialPageLoader>
         </DivColumn>
       </SectionedContainer>
-     )
+    );
   }
 }
+
+const mapStateToProps = state => {
+  return {
+    profileDetailsReducer: state.profileDetailsReducer
+  };
+};
+
+const mapDispathToProps = dispatch => {
+  return {
+    getProfileDetailsAction: bindActionCreators(
+      getProfileDetailsAction,
+      dispatch
+    )
+  };
+};
+
+export default connect(mapStateToProps, mapDispathToProps)(ProfileDetails);
