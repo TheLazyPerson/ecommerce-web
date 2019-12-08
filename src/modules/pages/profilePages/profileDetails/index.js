db980255caad84352f235ee5705dc2dc78e43b44
Kunal Pardeshi <kunalpardeshi280@gmail.com>
2019-12-08 11:38:52 +0530

    Fixed Profile Details render

--- src/modules/pages/profilePages/profileDetails/index.js
+++ src/modules/pages/profilePages/profileDetails/index.js
@@ -14,43 +14,11 @@ import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 
 class ProfileDetails extends Component {
-  state = {
-    userInfo: [
-      {
-        title: "First Name",
-        value: "Omar"
-      },
-      {
-        title: "Last Name",
-        value: "Lastname"
-      },
-      {
-        title: "Email Address",
-        value: "omarlastname@mail.com"
-      },
-      {
-        title: "Phone Number",
-        value: "+965-955-5836-852"
-      },
-      {
-        title: "Gender",
-        value: "NOTSET"
-      },
-      {
-        title: "Birthday",
-        value: "NOTSET"
-      }
-    ]
-  };
   render() {
-    const { userInfo } = this.state;
     const {
       profileDetailsReducer: { userDetails },
       getProfileDetailsAction
     } = this.props;
-
-    const user = userDetails.user_details;
-    console.log(userDetails);
 
     return (
       <SectionedContainer sideBarContainer={<SideNav />}>
@@ -60,20 +28,47 @@ class ProfileDetails extends Component {
               <SecondaryCapsuleButton className={styles.reset_password_button}>
                 Change Password
               </SecondaryCapsuleButton>
-
               <CapsuleButton>Edit Profile</CapsuleButton>
             </DivRow>
           </NavHeader>
 
           <InitialPageLoader initialPageApi={getProfileDetailsAction}>
+            {/* {user !== undefined && ( */}
             <DivColumn fillParent>
-              {map(userInfo, user => (
-                <DivColumn className={styles.field_container}>
-                  <div className={styles.title}>{user.title}</div>
-                  <div className={styles.value}>{user.value}</div>
-                </DivColumn>
-              ))}
+              <DivColumn className={styles.field_container}>
+                <div className={styles.title}>First Name</div>
+                <div className={styles.value}>{userDetails.first_name}</div>
+              </DivColumn>
+              <DivColumn className={styles.field_container}>
+                <div className={styles.title}>Last Name</div>
+                <div className={styles.value}>{userDetails.last_name}</div>
+              </DivColumn>
+              <DivColumn className={styles.field_container}>
+                <div className={styles.title}>Email</div>
+                <div className={styles.value}>{userDetails.email}</div>
+              </DivColumn>
+              <DivColumn className={styles.field_container}>
+                <div className={styles.title}>Phone Number</div>
+                <div className={styles.value}>
+                  {userDetails.phone ? userDetails.phone : "Not Available"}
+                </div>
+              </DivColumn>
+              <DivColumn className={styles.field_container}>
+                <div className={styles.title}>Gender</div>
+                <div className={styles.value}>
+                  {userDetails.gender ? userDetails.gender : "Not Available"}
+                </div>
+              </DivColumn>
+              <DivColumn className={styles.field_container}>
+                <div className={styles.title}>Birthday</div>
+                <div className={styles.value}>
+                  {userDetails.birthday
+                    ? userDetails.birthday
+                    : "Not Available"}
+                </div>
+              </DivColumn>
             </DivColumn>
+            {/* )} */}
           </InitialPageLoader>
         </DivColumn>
       </SectionedContainer>
