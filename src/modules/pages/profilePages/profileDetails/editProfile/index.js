e2400028d6f28f770865853adf1d3355b59a04ae
Kunal Pardeshi <kunalpardeshi280@gmail.com>
2019-12-10 23:13:18 +0530

    form validation to change password, edit profile

--- src/modules/pages/profilePages/profileDetails/editProfile/index.js
+++ src/modules/pages/profilePages/profileDetails/editProfile/index.js
@@ -1,59 +1,136 @@
-import React, { Component } from 'react';
-import SectionedContainer from 'CommonContainers/sectionedContainer';
-import DivColumn from 'CommonComponents/divColumn';
-import DivRow from 'CommonComponents/divRow';
-import SideNav from '../../components/sideNav';
-import styles from './edit_profile.module.scss';
-import NavHeader from '../../components/navHeader';
-import map from 'lodash/map';
-import CapsuleButton from 'CommonComponents/capsuleButton';
-import SecondaryCapsuleButton from 'CommonComponents/secondaryCapsuleButton';
-import InputTextComponent from 'CommonComponents/InputTextComponent';
-import navigatorHoc from 'Hoc/navigatorHoc';
+import React, { Component } from "react";
+import SectionedContainer from "CommonContainers/sectionedContainer";
+import DivRow from "CommonComponents/divRow";
+import SideNav from "../../components/sideNav";
+import styles from "./edit_profile.module.scss";
+import NavHeader from "../../components/navHeader";
+import { Form, Field } from "react-final-form";
+import CapsuleButton from "CommonComponents/capsuleButton";
+import SecondaryCapsuleButton from "CommonComponents/secondaryCapsuleButton";
+import InputTextComponent from "CommonComponents/InputTextComponent";
+import navigatorHoc from "Hoc/navigatorHoc";
+import {
+  isPhoneNumber,
+  nameValidator,
+  isEmptyValidator,
+  emailValidator
+} from "Utils/validators";
 
 class EditProfile extends Component {
-
-  onSubmit = (form) => {
-    form.preventDefault();
-    this.handleSubmitAction();
-  }
-
-  handleSubmitAction = () => {
-    console.log('onSubmitCalled');
-  }
-
-  onBackPress= () =>{
+  onBackPress = () => {
     const { pop } = this.props;
-
     pop();
-  }
+  };
 
-  onClickCancel= () => {
+  onSubmit = form => {
+    // TODO make Api call here
+  };
 
-  }
+  validate = values => {
+    const errors = {};
+    const validators = {
+      firstName: nameValidator(values.firstName),
+      lastName: nameValidator(values.lastName),
+      gender: isEmptyValidator(values.gender),
+      mobileNumber: isPhoneNumber(values.mobileNumber),
+      email: emailValidator(values.email),
+      birthday: isEmptyValidator(values.birthday)
+    };
+
+    Object.keys(validators).forEach(key => {
+      if (!validators[key].result) errors[key] = validators[key].error;
+    });
+
+    return errors;
+  };
+
+  onClickCancel = () => {
+    this.onBackPress();
+  };
 
   render() {
-
-     return (
-      <SectionedContainer
-       sideBarContainer={<SideNav />}
-      >
+    return (
+      <SectionedContainer sideBarContainer={<SideNav />}>
         <NavHeader title="Profile details" onBackClick={this.onBackPress} />
-          <form className={styles.form_container} onSubmit={this.onSubmit}>
-              <InputTextComponent placeholder="First Name" className={styles.input_text} />
-              <InputTextComponent placeholder="Last Name" className={styles.input_text} />
-              <InputTextComponent placeholder="Gender" className={styles.input_text} />
-              <InputTextComponent placeholder="Mobile Number" className={styles.input_text} />
-              <InputTextComponent placeholder="Email Address" className={styles.input_text} />
-              <InputTextComponent placeholder="Birthday" className={styles.input_text} />
+        <Form
+          onSubmit={this.onSubmit}
+          validate={this.validate}
+          render={({ handleSubmit, form, submitting, pristine, values }) => (
+            <form className={styles.form_container} onSubmit={handleSubmit}>
+              <Field name="firstName">
+                {({ input, meta }) => (
+                  <InputTextComponent
+                    meta={meta}
+                    {...input}
+                    placeholder="First Name"
+                    className={styles.input_text}
+                  />
+                )}
+              </Field>
+              <Field name="lastName">
+                {({ input, meta }) => (
+                  <InputTextComponent
+                    meta={meta}
+                    {...input}
+                    placeholder="Last Name"
+                    className={styles.input_text}
+                  />
+                )}
+              </Field>
+              <Field name="gender">
+                {({ input, meta }) => (
+                  <InputTextComponent
+                    meta={meta}
+                    {...input}
+                    placeholder="Gender"
+                    className={styles.input_text}
+                  />
+                )}
+              </Field>
+              <Field name="mobileNumber">
+                {({ input, meta }) => (
+                  <InputTextComponent
+                    meta={meta}
+                    {...input}
+                    placeholder="Mobile Number"
+                    className={styles.input_text}
+                  />
+                )}
+              </Field>
+              <Field name="email">
+                {({ input, meta }) => (
+                  <InputTextComponent
+                    meta={meta}
+                    {...input}
+                    placeholder="Email Address"
+                    className={styles.input_text}
+                  />
+                )}
+              </Field>
+              <Field name="birthday">
+                {({ input, meta }) => (
+                  <InputTextComponent
+                    meta={meta}
+                    {...input}
+                    placeholder="Birthday"
+                    className={styles.input_text}
+                  />
+                )}
+              </Field>
 
               <DivRow className={styles.form_button_container}>
-                <SecondaryCapsuleButton onClick={this.onClickCancel}>Cancel</SecondaryCapsuleButton>
-                <CapsuleButton onClick={this.handleSubmitAction}>Save Details</CapsuleButton>
+                <SecondaryCapsuleButton onClick={this.onClickCancel}>
+                  Cancel
+                </SecondaryCapsuleButton>
+                <CapsuleButton type="submit" disabled={submitting}>
+                  Save Details
+                </CapsuleButton>
               </DivRow>
-          </form>
+            </form>
+          )}
+        />
       </SectionedContainer>
-     )
+    );
   }
 }
 
