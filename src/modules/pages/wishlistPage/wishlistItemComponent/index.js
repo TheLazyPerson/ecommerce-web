acbda2dc8b7ce91000efdeeeea25db1543cd5f82
Riyaz Ahmed <ahmed.riyaz94@gmail.com>
2019-12-13 00:58:56 +0530

    implemented remove api with update code

--- src/modules/pages/wishlistPage/wishlistItemComponent/index.js
+++ src/modules/pages/wishlistPage/wishlistItemComponent/index.js
@@ -3,16 +3,43 @@ import DivRow from 'CommonComponents/divRow';
 import DivColumn from 'CommonComponents/divColumn';
 import styles from './wishlist_item_component.module.scss';
 import closeIcon from 'Icons/close-icon-black.svg';
-// import exhibitionImage1 from 'Images/exhibition-item-1.jpg';
+import { removeFromWishlist } from 'Core/modules/wishlist/wishlistActions';
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import { showSuccessFlashMessage } from 'Redux/actions/flashMessageActions';
 
-export default class WishlistItemComponent extends Component {
+class WishlistItemComponent extends Component {
+
+  state={
+    isRemoving: false
+  }
+
+  onClickRemove = () => {
+    const { wishlistItem, removeFromWishlist, showSuccessFlashMessage } = this.props;
+    
+    this.setState({ isRemoving: true });
+    removeFromWishlist(wishlistItem.id).then(({payload}) => {
+      if(payload.code == 200 || payload.code == 201) {
+        showSuccessFlashMessage('Product removed from wishlist')
+      }
+      this.setState({ isRemoving: false });
+    }).catch(error => {
+      this.setState({ isRemoving: false });
+    });
+  }
+
   render() {
     const { wishlistItem: { product } } = this.props;
+    const { isRemoving } = this.state;
 
     return (
-      <DivColumn className={styles.wishlist_item_container}>
+      <DivColumn className={`${styles.wishlist_item_container} ${isRemoving ? styles.is_disabled: null}`}>
         <DivColumn className={styles.wishlist_details_container}>
-          <img src={closeIcon} className={styles.close_icon} />
+          <img
+            src={closeIcon}
+            className={styles.close_icon}
+            onClick={!isRemoving ? this.onClickRemove: null}
+          />
           <div className={styles.name}>{product.name}</div>
           <div className={styles.description}>{product.short_description}</div>
           <img src={product.base_image.path} className={styles.image} />
@@ -32,3 +59,13 @@ export default class WishlistItemComponent extends Component {
     );
   }
 }
+
+
+const mapDispathToProps = dispatch => {
+  return {
+    removeFromWishlist: bindActionCreators(removeFromWishlist, dispatch),
+    showSuccessFlashMessage: bindActionCreators(showSuccessFlashMessage, dispatch),
+  };
+};
+
+export default connect(null, mapDispathToProps)(WishlistItemComponent);
