74abaa2383acc3426a51c008bc089bd62b863780
Riyaz Ahmed <ahmed.riyaz94@gmail.com>
2019-12-15 15:59:12 +0530

    navigates to exhibitionPage onClicking grid

--- src/modules/commonContainers/masonryGridContainer/index.js
+++ src/modules/commonContainers/masonryGridContainer/index.js
@@ -6,9 +6,17 @@ import DivRow from 'CommonComponents/divRow';
 import exhibitionImage from "Images/exhibition-item-3.png";
 import styles from "./masonry_grid_container.module.scss";
 import map from "lodash/map";
+import navigatorHoc from 'Hoc/navigatorHoc';
 
 class MasonryGridContainer extends Component {
   imageStates = [2, 1, 2, 1, 2, 2, 1];
+
+  onClickExhibitionItem = id => {
+    const { navigateTo } = this.props;
+    navigateTo("plp", {
+      id
+    });
+  };
 
   render() {
     const { exhibitionList } = this.props;
@@ -32,6 +40,7 @@ class MasonryGridContainer extends Component {
               <DivColumn
                 className={`${styles.grid_image_container} ${styles[`type${this.imageStates[typeIndex]}`]}`}
                 style={{ backgroundImage: `url(https://source.unsplash.com/500x50${typeIndex}/?product)` }}
+                onClick={()=> this.onClickExhibitionItem(exhibition.id)}
               >
                 <DivColumn fillParent className={styles.content_container}>
                   <div className={styles.title}>{exhibition.title}</div>
@@ -55,4 +64,4 @@ class MasonryGridContainer extends Component {
 // https://source.unsplash.com/500x500/?product
 MasonryGridContainer.propTypes = {};
 
-export default MasonryGridContainer;
+export default navigatorHoc(MasonryGridContainer);
