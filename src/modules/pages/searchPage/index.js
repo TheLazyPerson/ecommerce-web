aaa5b3d37341914688935775c9cdf3f1d49cff43
Riyaz Ahmed <ahmed.riyaz94@gmail.com>
2019-12-15 19:49:18 +0530

    makes api call based queryterm and searchType

--- src/modules/pages/searchPage/index.js
+++ src/modules/pages/searchPage/index.js
@@ -1,53 +1,104 @@
-import React, { Component } from 'react';
-import FullWidthContainer from 'CommonContainers/fullwidthContainer';
-import DivRow from 'CommonComponents/divRow';
-import DivColumn from 'CommonComponents/divColumn';
-import styles from './search_page.module.scss';
-import ExhibitionItemComponent from './exhibitionItemComponent';
-import ProductItemComponent from './productItemComponent';
+import React, { Component } from "react";
+import FullWidthContainer from "CommonContainers/fullwidthContainer";
+import DivRow from "CommonComponents/divRow";
+import DivColumn from "CommonComponents/divColumn";
+import styles from "./search_page.module.scss";
+import ExhibitionItemComponent from "./exhibitionItemComponent";
+import ProductItemComponent from "./productItemComponent";
+import InitialPageLoader from "CommonContainers/initialPageLoader";
+import { searchAction } from "Core/modules/search/searchActions";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import map from "lodash/map";
+import isEmpty from 'lodash/isEmpty';
+import queryString from "query-string";
+class SearchPage extends Component {
 
-export default class SearchPage extends Component {
+  getSlugBasedOntype = (searchType) => {
+    switch(searchType) {
+      case 'products':
+        return 'p';
+      case 'exhibitions':
+        return 'e';
+      default:
+        return 'all'
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    const {
+      match,
+      location,
+    } = this.props;
+    const currentSearchType = match.params.searchType;
+    const currentQuery = queryString.parse(location.search).query;
+
+  }
+
   render() {
+    const {
+      searchReducer: { productList, exhibitionList },
+      searchAction,
+      match,
+      location,
+    } = this.props;
+    const { searchType } = match.params;
+    const { query } = queryString.parse(location.search);
+
     return (
       <FullWidthContainer>
         <DivColumn fillParent className={styles.search_container}>
-         <div className={styles.page_header}>Search: the craft show</div>
-         <DivColumn fillParent className={styles.search_list_container}>
-
-          <DivColumn className={styles.section}>
-            <div className={styles.section_header}>
-              EXHIBITIONS
-            </div>
+          <div className={styles.page_header}>Search: the craft show</div>
+          <InitialPageLoader 
+            initialPageApi={() => searchAction(this.getSlugBasedOntype(searchType), query)}
+            isEmpty={(isEmpty(productList) && isEmpty(exhibitionList))}
+            emptyScreenMessage="Can't find any exhibition or product"
+          >
             
-            <DivRow className={styles.section_list}>
-              <ExhibitionItemComponent />
-              <ExhibitionItemComponent />
-              <ExhibitionItemComponent />
-              <ExhibitionItemComponent />
-              <ExhibitionItemComponent />
-            </DivRow>
-          </DivColumn>
+            <DivColumn fillParent className={styles.search_list_container}>
 
-          <DivColumn className={styles.section}>
-            <div className={styles.section_header}>
-              PRODUCTS
-            </div>
+              {!isEmpty(exhibitionList) && (
+                <DivColumn className={styles.section}>
+                  <div className={styles.section_header}>EXHIBITIONS</div>
+                  <DivRow className={styles.section_list}>
+                    {map(exhibitionList, exhibition => (
+                      <ExhibitionItemComponent />
+                    ))}
+                  </DivRow>
+                </DivColumn>
+              )}
 
-            <DivRow className={styles.section_list}>
-              <ProductItemComponent />
-              <ProductItemComponent />
-              <ProductItemComponent />
-              <ProductItemComponent />
-              <ProductItemComponent />
-              <ProductItemComponent />
-              <ProductItemComponent />
-            </DivRow>
-
-          </DivColumn>
-
-         </DivColumn>
+              {!isEmpty(productList) && (
+                <DivColumn className={styles.section}>
+                  <div className={styles.section_header}>PRODUCTS</div>
+                  <DivRow className={styles.section_list}>
+                    {map(productList, product => (
+                      <ProductItemComponent />
+                    ))}
+                  </DivRow>
+                </DivColumn>
+              )}
+            </DivColumn>
+          </InitialPageLoader>
         </DivColumn>
       </FullWidthContainer>
     );
   }
 }
+
+const mapStateToProps = state => {
+  return {
+    searchReducer: state.searchReducer
+  };
+};
+
+const mapDispathToProps = dispatch => {
+  return {
+    searchAction: bindActionCreators(searchAction, dispatch)
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispathToProps
+)(SearchPage);
