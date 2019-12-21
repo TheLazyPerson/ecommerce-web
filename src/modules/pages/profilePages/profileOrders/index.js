2dbd5b0c69cc11033d9c97addccb26ae7767a161
Riyaz Ahmed <ahmed.riyaz94@gmail.com>
2019-12-22 00:00:06 +0530

    created format helper for dates and implemented getOrderListApi

--- src/modules/pages/profilePages/profileOrders/index.js
+++ src/modules/pages/profilePages/profileOrders/index.js
@@ -6,50 +6,92 @@ import SideNav from "../components/sideNav";
 import styles from "./profile_orders.module.scss";
 import exhibitionImage1 from "Images/exhibition-item-1.jpg";
 import navigatorHoc from "Hoc/navigatorHoc";
+import { getOrderListAction } from "Core/modules/order/orderActions";
+import isEmpty from "lodash/isEmpty";
+import map from "lodash/map";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import InitialPageLoader from "CommonContainers/initialPageLoader";
+import { formatTimeStamp, timeFormats } from "Utils/formatHelper";
 
 class ProfileOrders extends Component {
-  onClickProductViewDetails = () => {
+  onClickProductViewDetails = (orderId) => {
     const { navigateTo } = this.props;
-    navigateTo("order-details");
+    navigateTo("order-details", {orderId});
   };
 
   render() {
+    const {
+      orderReducer: { orderList },
+      getOrderListAction
+    } = this.props;
+
     return (
       <SectionedContainer sideBarContainer={<SideNav />}>
-        <DivColumn fillParent className={styles.page_container}>
+        <InitialPageLoader
+          initialPageApi={getOrderListAction}
+          isEmpty={isEmpty(orderList)}
+        >
+          <DivColumn fillParent className={styles.page_container}>
 
-          <DivColumn className={styles.orders_container}>
-            <DivRow className={styles.list_header}>
-              <div className={styles.header_text}>
-                ORDER DATE: <b>3 sep, 2019</b>
-              </div>
-              <div className={styles.header_text}>
-                ORDER ID: <b>4564545</b>
-              </div>
-            </DivRow>
-            <DivRow className={styles.order_item_container}>
-              <img className={styles.order_image} src={exhibitionImage1} />
-              <DivColumn className={styles.order_detail_container}>
-                <div className={styles.order_exhibition}>
-                  Handcrafted Watches, Products
-                </div>
-                <div className={styles.order_name}>Perfumes</div>
-                <div className={styles.order_price}>KD 3.99</div>
-                <div className={styles.order_state}>DELIVERED</div>
-                <div
-                  className={styles.view_order_button}
-                  onClick={this.onClickProductViewDetails}
-                >
-                  View details
-                </div>
+            {map(orderList, order => (
+              <DivColumn className={styles.orders_container}>
+                <DivRow className={styles.list_header}>
+                  <div className={styles.header_text}>
+                    ORDER DATE:{" "}
+                    <b>{formatTimeStamp(order.created_at, timeFormats.dayMonthComaYear)}</b>
+                  </div>
+                  <div className={styles.header_text}>
+                    ORDER ID: <b>{order.id}</b>
+                  </div>
+                </DivRow>
+
+                {map(order.items, item => (
+                  <DivRow className={styles.order_item_container}>
+                    <img
+                      className={styles.order_image}
+                      src={exhibitionImage1}
+                    />
+                    <DivColumn className={styles.order_detail_container}>
+                      <div className={styles.order_exhibition}>
+                        {item.type}
+                      </div>
+                      <div className={styles.order_name}>Perfumes</div>
+                      <div className={styles.order_price}>KD 3.99</div>
+                      <div className={styles.order_state}>DELIVERED</div>
+                      <div
+                        className={styles.view_order_button}
+                        onClick={()=>this.onClickProductViewDetails(order.id)}
+                      >
+                        View details
+                      </div>
+                    </DivColumn>
+                  </DivRow>
+                ))}
+
               </DivColumn>
-            </DivRow>
-          </DivColumn>
+            ))}
 
-        </DivColumn>
+          </DivColumn>
+        </InitialPageLoader>
       </SectionedContainer>
     );
   }
 }
 
-export default navigatorHoc(ProfileOrders);
+const mapStateToProps = state => {
+  return {
+    orderReducer: state.orderReducer
+  };
+};
+
+const mapDispathToProps = dispatch => {
+  return {
+    getOrderListAction: bindActionCreators(getOrderListAction, dispatch)
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispathToProps
+)(navigatorHoc(ProfileOrders));
