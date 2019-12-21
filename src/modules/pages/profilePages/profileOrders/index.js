a31a6b2f96d3657382f2a4c21b468618927bb122
Riyaz Ahmed <ahmed.riyaz94@gmail.com>
2019-12-21 23:16:05 +0530

    minor restructuring to order listing page

--- src/modules/pages/profilePages/profileOrders/index.js
+++ src/modules/pages/profilePages/profileOrders/index.js
@@ -1,43 +1,54 @@
-import React, { Component } from 'react';
-import SectionedContainer from 'CommonContainers/sectionedContainer';
-import DivColumn from 'CommonComponents/divColumn';
-import DivRow from 'CommonComponents/divRow';
-import SideNav from '../components/sideNav';
-import styles from './profile_orders.module.scss';
-import exhibitionImage1 from 'Images/exhibition-item-1.jpg';
-import navigatorHoc from 'Hoc/navigatorHoc';
+import React, { Component } from "react";
+import SectionedContainer from "CommonContainers/sectionedContainer";
+import DivColumn from "CommonComponents/divColumn";
+import DivRow from "CommonComponents/divRow";
+import SideNav from "../components/sideNav";
+import styles from "./profile_orders.module.scss";
+import exhibitionImage1 from "Images/exhibition-item-1.jpg";
+import navigatorHoc from "Hoc/navigatorHoc";
 
 class ProfileOrders extends Component {
   onClickProductViewDetails = () => {
     const { navigateTo } = this.props;
-    navigateTo('order-details');
-  }
+    navigateTo("order-details");
+  };
 
   render() {
-     return (
-      <SectionedContainer
-        sideBarContainer={<SideNav />}
-      >
-        <DivColumn className={styles.orders_container}>
-          <DivRow className={styles.list_header}>
-            
-            <div className={styles.header_text}>ORDER DATE: <b>3 sep, 2019</b></div>
-            <div className={styles.header_text}>ORDER ID: <b>4564545</b></div>
+    return (
+      <SectionedContainer sideBarContainer={<SideNav />}>
+        <DivColumn fillParent className={styles.page_container}>
 
-          </DivRow>
-          <DivRow className={styles.order_item_container}>
-            <img className={styles.order_image} src={exhibitionImage1}/>
-            <DivColumn className={styles.order_detail_container}>
-              <div className={styles.order_exhibition}>Handcrafted Watches, Products</div>             
-              <div className={styles.order_name}>Perfumes</div>
-              <div className={styles.order_price}>KD 3.99</div>
-              <div className={styles.order_state}>DELIVERED</div>
-              <div className={styles.view_order_button} onClick={this.onClickProductViewDetails}>View details</div>
-            </DivColumn>
-          </DivRow>
+          <DivColumn className={styles.orders_container}>
+            <DivRow className={styles.list_header}>
+              <div className={styles.header_text}>
+                ORDER DATE: <b>3 sep, 2019</b>
+              </div>
+              <div className={styles.header_text}>
+                ORDER ID: <b>4564545</b>
+              </div>
+            </DivRow>
+            <DivRow className={styles.order_item_container}>
+              <img className={styles.order_image} src={exhibitionImage1} />
+              <DivColumn className={styles.order_detail_container}>
+                <div className={styles.order_exhibition}>
+                  Handcrafted Watches, Products
+                </div>
+                <div className={styles.order_name}>Perfumes</div>
+                <div className={styles.order_price}>KD 3.99</div>
+                <div className={styles.order_state}>DELIVERED</div>
+                <div
+                  className={styles.view_order_button}
+                  onClick={this.onClickProductViewDetails}
+                >
+                  View details
+                </div>
+              </DivColumn>
+            </DivRow>
+          </DivColumn>
+
         </DivColumn>
       </SectionedContainer>
-     )
+    );
   }
 }
 
