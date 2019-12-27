8debd84867af0f8b36c3603b93344acf53965ae3
Riyaz Ahmed <ahmed.riyaz94@gmail.com>
2019-12-27 15:03:16 +0530

    translated home page with header and footer

--- src/modules/commonComponents/pageFooter/index.js
+++ src/modules/commonComponents/pageFooter/index.js
@@ -4,35 +4,38 @@ import DivRow from 'CommonComponents/divRow';
 import styles from './page_footer.module.scss';
 import playStoreImage from 'Images/google-play-badge.png';
 import appStoreImage from 'Images/app-store-badge.svg';
+import translatorHoc from 'Hoc/translatorHoc';
 
-export default class PageFooter extends Component {
+class PageFooter extends Component {
   render() {
+    const { translate } = this.props;
+
     return (
       <DivColumn fillSelfHorizontal className={styles.footer_container}>
        <DivRow className={styles.footer_content}>
         <DivColumn className={styles.footer_link_container}>
-          <b className={styles.link_header}>APP</b>
-          <a className={styles.link} href="/">Home</a>
-          <a className={styles.link} href="/profile">Profile</a>
-          <a className={styles.link} href="/wishlist">Wishlist</a>
-          <a className={styles.link} href="/profile/heplcenter">HelpCenter</a>
-          <a className={styles.link} href="/profile/settings">Settings</a>
+          <b className={styles.link_header}>{translate('footer.title_app')}</b>
+          <a className={styles.link} href="/">{translate('footer.home')}</a>
+          <a className={styles.link} href="/profile">{translate('footer.profile')}</a>
+          <a className={styles.link} href="/wishlist">{translate('footer.wishlist')}</a>
+          <a className={styles.link} href="/profile/heplcenter">{translate('footer.helpcenter')}</a>
+          <a className={styles.link} href="/profile/settings">{translate('footer.settings')}</a>
         </DivColumn>
         <DivColumn className={styles.footer_link_container}>
-          <b className={styles.link_header}>USEFUL LINKS</b>
-          <a className={styles.link} href="/faq">Faq</a>
-          <a className={styles.link} href="/privacy-policy">Privacy Policy</a>
-          <a className={styles.link} href="/terms-and-condition">Terms and Condition</a>
-          <a className={styles.link} href="/shipping-and-returns">Shipping and Returns</a>
+          <b className={styles.link_header}>{translate('footer.title_link')}</b>
+          <a className={styles.link} href="/faq">{translate('footer.faq')}</a>
+          <a className={styles.link} href="/privacy-policy">{translate('footer.privacy_policy')}</a>
+          <a className={styles.link} href="/terms-and-condition">{translate('footer.terms')}</a>
+          <a className={styles.link} href="/shipping-and-returns">{translate('footer.shipping')}</a>
         </DivColumn>
         <DivColumn className={styles.footer_link_container}>
-          <b className={styles.link_header}>EXPERIENCE MA3RATH APP ON MOBILE</b>
+          <b className={styles.link_header}>{translate('footer.experience')}</b>
           <DivRow verticalCenter>
            <img className={styles.badge_icon} src={playStoreImage} />
            <img className={styles.apple_badge} src={appStoreImage} />
           </DivRow>
 
-          <b className={styles.link_header} style={{marginTop: 16}}>KEEP IN TOUCH</b>
+          <b className={styles.link_header} style={{marginTop: 16}}>{translate('footer.keep_in_touch')}</b>
           <DivRow>
             <a className={styles.facebook}></a>
             <a className={styles.twitter}></a>
@@ -45,3 +48,6 @@ export default class PageFooter extends Component {
     );
   }
 }
+
+
+export default translatorHoc(PageFooter);
