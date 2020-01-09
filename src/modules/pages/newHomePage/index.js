c599564fdafe784241fae4cc0adcd6f23c16e03f
Riyaz <ahmed.riyaz94@gmail.com>
2020-01-09 19:12:52 +0530

    included home title responsive

--- src/modules/pages/newHomePage/index.js
+++ src/modules/pages/newHomePage/index.js
@@ -218,7 +218,7 @@ class NewHomePage extends Component {
           </InitialPageLoader>
         </DivColumn>
 
-        {/* <DivRow horizontalCenter fillSelfHorizontal className={styles.header_title}>
+        <DivRow horizontalCenter fillSelfHorizontal className={styles.header_title}>
           {translate('home_page.trending_now_title')}
         </DivRow>
 
@@ -229,7 +229,7 @@ class NewHomePage extends Component {
           />
         </InitialPageLoader>
 
-        <DivRow horizontalCenter fillSelfHorizontal className={styles.header_title}>
+        {/* <DivRow horizontalCenter fillSelfHorizontal className={styles.header_title}>
           {translate('home_page.upcoming_title')}
         </DivRow>
 
