5115971613d883125bee0f6832658de1fbc96d3c
Riyaz <ahmed.riyaz94@gmail.com>
2020-01-10 22:05:34 +0530

    rtl implemented on asonry layout

--- src/modules/hoc/translatorHoc.js
+++ src/modules/hoc/translatorHoc.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { withTranslation, Trans } from 'react-i18next';
+import { connect } from "react-redux";
 
 const translatorHoc = WrappedComponent => {
   class translator extends Component {
@@ -10,16 +11,29 @@ const translatorHoc = WrappedComponent => {
     }
 
     render() {
+      const {
+        languageReducer: {
+          languageCode,
+        }
+      } = this.props;
+
       return (
         <WrappedComponent
           {...this.props}
           translate={this.translate}
+          isRTL={languageCode == 'ar'}
         />
       );
     }
   }
 
-  return withTranslation('common')(translator);
+  const mapStateToProps = (state) => {
+    return {
+      languageReducer: state.languageReducer,
+    }
+  }
+
+  return connect(mapStateToProps, null)(withTranslation('common')(translator));
 };
 
 export default translatorHoc;
