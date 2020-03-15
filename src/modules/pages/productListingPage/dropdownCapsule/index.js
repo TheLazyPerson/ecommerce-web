4df65d9cfdfe39584a30814c0c8d4b2306e3066c
Riyaz <ahmed.riyaz94@gmail.com>
2020-03-16 00:34:18 +0530

    included dropdown capsule in product listing page

--- src/modules/pages/productListingPage/dropdownCapsule/index.js
+++ src/modules/pages/productListingPage/dropdownCapsule/index.js
@@ -1,17 +1,44 @@
 import React, { Component } from 'react';
 import styles from './dropdown_capsule.module.scss';
 import DivRow from 'CommonComponents/divRow';
+// import arrowDownIcon from 'Icons/arrow-down-icon-black.svg';
+import Select from 'react-select';
 
 export default class DropdownCapsule extends Component {
+
+  getDropdownValue = () => ([
+    {
+      value: 'Name',
+      label: 'Name'
+    },
+    {
+      value: 'Price',
+      label: 'Price'
+    },
+    {
+      value: 'Discount',
+      label: 'Discount'
+    }
+  ])
+
   render() {
     return (
       <DivRow
-        verticalCenter
-        horizontalCenter 
         className={styles.capsule_container}
       >
-       <div className={styles.capsule_text}>Sort by</div>
-       <span className={styles.close_icon}>></span>
+
+        <Select
+          className="dropdown-container"
+          classNamePrefix="dropdown"
+          name="sort"
+          placeholder="Sort"
+          options={this.getDropdownValue()}
+        />
+        {/* <div className={styles.capsule_text}>Sort by</div>
+        <img
+          src={arrowDownIcon}
+          className={styles.close_icon}
+        /> */}
       </DivRow>
     );
   }
