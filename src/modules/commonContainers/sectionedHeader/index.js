bf9b9d56ff34b808aabe82e02c42ff7efeca1655
Riyaz <ahmed.riyaz94@gmail.com>
2020-03-15 23:51:51 +0530

    working login logout action in header

--- src/modules/commonContainers/sectionedHeader/index.js
+++ src/modules/commonContainers/sectionedHeader/index.js
@@ -10,14 +10,20 @@ import bagIconWhite from "Icons/cart-bag-icon-white.svg";
 import bookmarkIcon from "Icons/bookmark-icon-black.svg";
 import bookmarkIconWhite from "Icons/bookmark-icon-white.svg";
 import arrowDownIcon from "Icons/arrow-down-icon-black.svg";
+import { logoutAction } from "Core/modules/signin/actions/signinActions";
 import navigatorHoc from "Hoc/navigatorHoc";
 import profileIconBlack from "Icons/profile-icon-black.svg";
 import profileIconWhite from "Icons/profile-icon-white.svg";
 import HorizontalBorder from "CommonComponents/horizontalBorder";
 import { searchTypes } from "Constants/searchConstants";
 import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import { CookieService } from "Utils/cookieService";
+import { USER_DATA_COOKIE } from "Constants/cookieConstants";
 import translatorHoc from 'Hoc/translatorHoc';
+import { setBagCount } from 'Core/modules/bag/bagActions';
 import SearchBar from "../searchBar";
+
 
 class SectionedHeader extends Component {
   clickedOnSearchItem = false;
@@ -42,6 +48,21 @@ class SectionedHeader extends Component {
     navigateTo("checkout");
   };
 
+  onClickLoginOrLogoutHandler = () => {
+    const { logoutAction, isUserSignedIn, navigateTo, setBagCount } = this.props;
+
+    if (isUserSignedIn) {
+      logoutAction().then(() => {
+        CookieService.delete(USER_DATA_COOKIE);
+        CookieService.delete('BAG_COUNT');
+        setBagCount(0);
+        navigateTo(""); // ToHomePage
+      });
+    } else {
+      navigateTo('signin')
+    }
+  }
+
   render() {
     const { isUserSignedIn, bagCount, whiteColor, translate } = this.props;
 
@@ -53,7 +74,7 @@ class SectionedHeader extends Component {
         />
 
         <DivRow verticalCenter>
-          <DivRow
+          {/* <DivRow
             className={styles.header_item_container}
             verticalCenter
             onClick={this.onClickBag}
@@ -69,13 +90,25 @@ class SectionedHeader extends Component {
             >
               {bagCount != null ? bagCount : 0}
             </DivRow>
-          </DivRow>
-          <img
+          </DivRow> */}
+          {/* <img
             className={`${styles.header_icon} ${styles.header_item_container}`}
             src={whiteColor ? bookmarkIconWhite : bookmarkIcon}
             onClick={this.onClickWishlist}
-          />
-          {isUserSignedIn ? (
+          /> */}
+
+          <div
+            style={{ height: "unset" }}
+            className={`${styles.header_icon} ${styles.header_item_container} ${styles.profile_header_item}`}
+          >
+            <img
+              className={styles.profile_pic}
+              src={whiteColor ? profileIconWhite : profileIconBlack}
+            />
+            {/* <img src={arrowDownIcon} className={styles.arrow_down_icon} /> */}
+          </div>
+
+          {/* {isUserSignedIn ? (
             <div
               style={{ height: "unset" }}
               className={`${styles.header_icon} ${styles.header_item_container} ${styles.profile_header_item}`}
@@ -85,7 +118,6 @@ class SectionedHeader extends Component {
                 className={styles.profile_pic}
                 src={whiteColor ? profileIconWhite : profileIconBlack}
               />
-              {/* <img src={arrowDownIcon} className={styles.arrow_down_icon} /> */}
             </div>
           ) : (
               <a
@@ -96,8 +128,8 @@ class SectionedHeader extends Component {
               >
                 {translate('header.login')}
               </a>
-            )}
-          {!isUserSignedIn && (
+            )} */}
+          {/* {!isUserSignedIn && (
             <a
               className={`${styles.sigin_link} ${
                 styles.header_item_container
@@ -106,22 +138,46 @@ class SectionedHeader extends Component {
             >
               {translate('header.register')}
             </a>
-          )}
+          )} */}
           {/* <img src={hamburgerMenuIcon} className={`${styles.hamburger_icon} ${styles.header_item_container}`} /> */}
           <DivColumn className={styles.header_overlay}>
-            <div className={styles.header_item}>My Profile</div>
-            <DivRow style={{ justifyContent: 'space-between' }} className={styles.header_item}>
-              Bag
+            <div
+              className={styles.header_item}
+              onClick={this.onClickProfile}
+            >
+              <div className={styles.text}>My Profile</div>
+
+            </div>
+            <DivRow
+              style={{ justifyContent: 'space-between' }}
+              className={styles.header_item}
+              onClick={this.onClickBag}
+            >
+              <div className={styles.text}>Bag</div>
+
               <DivRow
                 verticalCenter
                 horizontalCenter
                 className={styles.bag_count}
               >
-                2
+                {bagCount != null ? bagCount : 0}
               </DivRow>
             </DivRow>
-            <div className={styles.header_item}>Wishlist</div>
-            <div className={styles.header_item}>Logout/login</div>
+            <div
+              className={styles.header_item}
+              onClick={this.onClickWishlist}
+            >
+              <div className={styles.text}>{translate('common.wishlist')}</div>
+
+            </div>
+            <div
+              className={styles.header_item}
+              onClick={this.onClickLoginOrLogoutHandler}
+            >
+              <div className={styles.text}>{
+                isUserSignedIn ? translate('header.logout') : translate('header.login')
+              }</div>
+            </div>
           </DivColumn>
         </DivRow>
       </DivRow>
@@ -136,4 +192,11 @@ const mapStateToProps = state => {
   };
 };
 
-export default connect(mapStateToProps, null)(translatorHoc(navigatorHoc(SectionedHeader)));
+const mapDispathToProps = dispatch => {
+  return {
+    logoutAction: bindActionCreators(logoutAction, dispatch),
+    setBagCount: bindActionCreators(setBagCount, dispatch),
+  };
+};
+
+export default connect(mapStateToProps, mapDispathToProps)(translatorHoc(navigatorHoc(SectionedHeader)));
