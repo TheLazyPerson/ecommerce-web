90dc63b461e179c54c4307594c1734383ae65014
Riyaz <ahmed.riyaz94@gmail.com>
2020-03-19 22:26:27 +0530

    working range filter

--- src/modules/pages/productListingPage/sideBarFilter/index.js
+++ src/modules/pages/productListingPage/sideBarFilter/index.js
@@ -7,11 +7,9 @@ import translatorHoc from "Hoc/translatorHoc";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import map from "lodash/map";
+import filter from 'lodash/filter';
 import {
-  getProductListAction,
-  addFilters,
   setFilters,
-  clearFilters
 } from "Core/modules/productlist/productListActions";
 import "rc-slider/assets/index.css";
 import "rc-tooltip/assets/bootstrap.css";
@@ -19,15 +17,38 @@ import Slider from "rc-slider";
 
 const createSliderWithTooltip = Slider.createSliderWithTooltip;
 const Range = createSliderWithTooltip(Slider.Range);
+
 class SideBarFilter extends Component {
   onChangeCheckboxFilterItem = (event, facet) => {
-    const { makeApiCall } = this.props;
-    makeApiCall();
+    const { makeApiCall, productListReducer, setFilters } = this.props;
+    const checkBoxValue = event.target.checked;
+    const reducerFilter = productListReducer.filters;
+
+    const filters = checkBoxValue ? new Set(reducerFilter[facet.key] ? [...reducerFilter[facet.key], facet.id] : [facet.id])
+      : filter(reducerFilter[facet.key], facetId => facetId != facet.id)
+
+    let updatedFilter = {
+      ...reducerFilter,
+      [facet.key]: [...filters] // converting set to array    
+    };
+
+    setFilters(updatedFilter);
+    setTimeout(() => makeApiCall(), 100);
   };
+
   onChangeRange = (event, facet) => {
-    const { makeApiCall } = this.props;
-    console.log(event);
-    makeApiCall();
+    const { makeApiCall, productListReducer, setFilters } = this.props;
+    const reducerFilter = productListReducer.filters;
+    const updatedFilter = {
+      ...reducerFilter,
+      [facet.key]: {
+        min_price: event[0],
+        max_price: event[1],
+      }
+    };
+
+    setFilters(updatedFilter);
+    setTimeout(() => makeApiCall(), 100);
   };
 
   render() {
@@ -62,15 +83,15 @@ class SideBarFilter extends Component {
                       text={facet.name}
                       textStyle={styles.checkbox_text}
                       onChange={event =>
-                        this.onChangeCheckboxFilterItem(event, facet)
+                        this.onChangeCheckboxFilterItem(event, { ...facet, key: displayFilter.key })
                       }
                     />
                   ))}
                 {displayFilter.type == "multi-select" && displayFilter.facets && (
                   <Fragment>
                     <Range
-                      onChange={event =>
-                        this.onChangeRange(event, displayFilter.facets)
+                      onAfterChange={event =>
+                        this.onChangeRange(event, { ...displayFilter.facets, key: displayFilter.key })
                       }
                       min={displayFilter.facets.min_price}
                       max={displayFilter.facets.max_price}
@@ -107,7 +128,7 @@ const mapStateToProps = state => {
 
 const mapDispathToProps = dispatch => {
   return {
-    getProductListAction: bindActionCreators(getProductListAction, dispatch)
+    setFilters: bindActionCreators(setFilters, dispatch)
   };
 };
 
