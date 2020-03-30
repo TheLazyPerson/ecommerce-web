7100a52287b524ea9bed4391437e83bfdd641a27
Om Komawar <omkomawar222@gmail.com>
2020-03-30 11:33:54 +0530

    added users name in profile overview

--- src/modules/pages/profilePages/index.js
+++ src/modules/pages/profilePages/index.js
@@ -12,7 +12,8 @@ import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import { CookieService } from "Utils/cookieService";
 import { USER_DATA_COOKIE } from "Constants/cookieConstants";
-import { setBagCount } from 'Core/modules/bag/bagActions';
+import { setBagCount } from "Core/modules/bag/bagActions";
+import isEmpty from "lodash/isEmpty";
 
 class ProfileOverview extends Component {
   onClickNavItemClick = slug => {
@@ -25,7 +26,7 @@ class ProfileOverview extends Component {
     } else if (slug === "logout") {
       logoutAction().then(() => {
         CookieService.delete(USER_DATA_COOKIE);
-        CookieService.delete('BAG_COUNT');
+        CookieService.delete("BAG_COUNT");
         setBagCount(0);
         navigateTo(""); // ToHomePage
       });
@@ -35,6 +36,9 @@ class ProfileOverview extends Component {
   };
 
   render() {
+    const {
+      signInReducer: { userDetails }
+    } = this.props;
     return (
       <SectionedContainer sideBarContainer={<SideNav />}>
         <DivColumn className={styles.profile_overview_container}>
@@ -44,7 +48,9 @@ class ProfileOverview extends Component {
             className={styles.header_container}
           >
             <div className={styles.header_title}>MY ACCOUNT</div>
-            <div className={styles.header_message}>Welcome, Omar.</div>
+            <div className={styles.header_message}>
+              Welcome, {!isEmpty(userDetails) && userDetails.first_name}.
+            </div>
           </DivColumn>
 
           <DivRow className={styles.items_container}>
@@ -78,11 +84,20 @@ class ProfileOverview extends Component {
   }
 }
 
-const mapDispathToProps = dispatch => {
+const mapStateToProps = state => {
   return {
-    logoutAction: bindActionCreators(logoutAction, dispatch),
-    setBagCount: bindActionCreators(setBagCount, dispatch),
+    signInReducer: state.signInReducer
   };
 };
 
-export default connect(null, mapDispathToProps)(navigatorHoc(ProfileOverview));
+const mapDispathToProps = dispatch => {
+  return {
+    logoutAction: bindActionCreators(logoutAction, dispatch),
+    setBagCount: bindActionCreators(setBagCount, dispatch)
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispathToProps
+)(navigatorHoc(ProfileOverview));
