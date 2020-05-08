0ae1ef3044180a5c0ab146baa31efaed2b31879d
Om Komawar <omkomawar222@gmail.com>
2020-05-08 18:43:57 +0530

    login - added react form integration and validations

--- src/modules/pages/signinPage/index.js
+++ src/modules/pages/signinPage/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import FullWidthContainer from "CommonContainers/fullwidthContainer";
 import DivColumn from "CommonComponents/divColumn";
 import styles from "./signin_page.module.scss";
@@ -10,35 +10,42 @@ import { bindActionCreators } from "redux";
 import { postSigninAction } from "Core/modules/signin/actions/signinActions";
 import { CookieService } from "Utils/cookieService";
 import { USER_DATA_COOKIE } from "Constants/cookieConstants";
-import translatorHoc from 'Hoc/translatorHoc';
+import translatorHoc from "Hoc/translatorHoc";
+import { emailValidator, isEmptyValidator } from "Utils/validators";
+import { Form, Field } from "react-final-form";
 
 class SignInPage extends Component {
-  state = {
-    userName: "",
-    password: ""
+  onSubmit = (form) => {
+    const { postSigninAction, navigateTo } = this.props;
+
+    postSigninAction({
+      email: form.userName,
+      password: form.password,
+    }).then(({ payload }) => {
+      const { data, code } = payload;
+
+      if (code == 200 || code == 201) {
+        CookieService.set(USER_DATA_COOKIE, data);
+        navigateTo("");
+      }
+    });
   };
 
-  onSubmit = form => {
-    form.preventDefault();
-    const { postSigninAction, navigateTo } = this.props;
-    const { userName, password } = this.state;
+  validate = (values) => {
+    const errors = {};
+    const validators = {
+      userName: emailValidator(values.userName),
+      password: isEmptyValidator(values.password),
+    };
 
-    if (userName && password) {
-      postSigninAction({
-        email: userName, // "buisness@gmail.com",
-        password
-      }).then(response => {
-        const { data, code } = response.payload;
-        if (code === 200 || code === 201) {
-          CookieService.set(USER_DATA_COOKIE, data);
-          navigateTo("");
-        }
-      });
-    }
+    Object.keys(validators).forEach((key) => {
+      if (!validators[key].result) errors[key] = validators[key].error;
+    });
+
+    return errors;
   };
 
   render() {
-    const { userName, password } = this.state;
     const { translate } = this.props;
 
     return (
@@ -48,50 +55,77 @@ class SignInPage extends Component {
           horizontalCenter
           className={styles.page_container}
         >
-          <div className={styles.signin_title_text}>{translate('signin_page.page_title')}</div>
-          <form className={styles.form_container} onSubmit={this.onSubmit}>
-            <InputTextComponent
-              placeholder={translate('signin_page.username')}
-              className={styles.input_text}
-              value={userName}
-              onChange={event =>
-                this.setState({ userName: event.target.value })
-              }
-            />
-
-            <InputTextComponent
-              placeholder={translate('signin_page.password')}
-              className={styles.input_text}
-              type="password"
-              value={password}
-              onChange={event =>
-                this.setState({ password: event.target.value })
-              }
-            />
-
-            <InputCheckbox text={translate('signin_page.remember_me')} />
-            <input
-              type="submit"
-              value={translate('signin_page.sign_in_button')}
-              className={styles.input_submit}
-            />
-          </form>
+          <div className={styles.signin_title_text}>
+            {translate("signin_page.page_title")}
+          </div>
+          <Form
+            onSubmit={this.onSubmit}
+            validate={this.validate}
+            render={({ handleSubmit, form, submitting, pristine, values }) => (
+              <form className={styles.form_container} onSubmit={handleSubmit}>
+                <Fragment>
+                  <Field name="userName">
+                    {({ input, meta }) => (
+                      <InputTextComponent
+                        meta={meta}
+                        {...input}
+                        placeholder={translate("signin_page.username")}
+                        className={styles.input_text}
+                      />
+                    )}
+                  </Field>
+                  <Field name="password">
+                    {({ input, meta }) => (
+                      <InputTextComponent
+                        meta={meta}
+                        {...input}
+                        placeholder={translate("signin_page.password")}
+                        className={styles.input_text}
+                        type="password"
+                      />
+                    )}
+                  </Field>
+                  <Field name="rememberMe">
+                    {({ input, meta }) => (
+                      <InputCheckbox
+                        meta={meta}
+                        {...input}
+                        text={translate("signin_page.remember_me")}
+                      />
+                    )}
+                  </Field>
+                </Fragment>
+                <input
+                  type="submit"
+                  value={translate("signin_page.sign_in_button")}
+                  className={styles.input_submit}
+                  disabled={submitting}
+                />
+              </form>
+            )}
+          />
           {/* <a className={styles.hyper_link} href="/forgot-password">
             Forgot password
           </a> */}
           <div className={styles.create_account_container}>
             <span className={styles.new_description_text}>
-              {translate('signin_page.new')}&nbsp;
+              {translate("signin_page.new")}&nbsp;
             </span>
             <a className={styles.hyper_link} href="/signup">
-              {translate('signin_page.create')}
+              {translate("signin_page.create")}
             </a>
           </div>
-          <div style={{ marginTop: 26 }} className={styles.create_account_container}>
+          <div
+            style={{ marginTop: 26 }}
+            className={styles.create_account_container}
+          >
             <span className={styles.new_description_text}>
               Are you a seller?&nbsp;
             </span>
-            <a className={styles.hyper_link} href="http://ec2-15-206-82-110.ap-south-1.compute.amazonaws.com/">
+            <a
+              className={styles.hyper_link}
+              href="http://ec2-15-206-82-110.ap-south-1.compute.amazonaws.com/"
+            >
               Create a seller account
             </a>
           </div>
@@ -101,15 +135,15 @@ class SignInPage extends Component {
   }
 }
 
-const mapStateToProps = state => {
+const mapStateToProps = (state) => {
   return {
-    signInReducer: state.signInReducer
+    signInReducer: state.signInReducer,
   };
 };
 
-const mapDispathToProps = dispatch => {
+const mapDispathToProps = (dispatch) => {
   return {
-    postSigninAction: bindActionCreators(postSigninAction, dispatch)
+    postSigninAction: bindActionCreators(postSigninAction, dispatch),
   };
 };
 
