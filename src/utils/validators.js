aca97be193309eb2ab55398e03807efb3295c899
Om Komawar <omkomawar222@gmail.com>
2020-05-12 22:29:42 +0530

    updated the validation message

--- src/utils/validators.js
+++ src/utils/validators.js
@@ -1,33 +1,33 @@
-import isEmpty from 'lodash/isEmpty';
+import isEmpty from "lodash/isEmpty";
 
-export const nameValidator = value => {
+export const nameValidator = (value) => {
   const nameRegex = /^[A-Za-z]+$/;
   let error = "";
 
-  if (!value) error = "Required";
+  if (!value) error = "Please fill in these details to continue";
   else if (!value.match(nameRegex))
     error = "Special characters and numbers not allowed";
 
   return error
     ? {
         result: false,
-        error
+        error,
       }
     : { result: true };
 };
 
-export const emailValidator = value => {
+export const emailValidator = (value) => {
   const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
   let error = "";
 
-  if (!value) error = "Required";
+  if (!value) error = "Please fill in these details to continue";
   else if (!emailRegex.test(value))
     error = "Please provide a valid email address";
 
   return error
     ? {
         result: false,
-        error
+        error,
       }
     : { result: true };
 };
@@ -35,39 +35,39 @@ export const emailValidator = value => {
 export const passwordValidator = (password, confirmPassword) => {
   let error = "";
 
-  if (!confirmPassword) error = "Required";
+  if (!confirmPassword) error = "Please fill in these details to continue";
   else if (password !== confirmPassword) error = "Password did not match";
 
   return error
     ? {
         result: false,
-        error
+        error,
       }
     : { result: true };
 };
 
-export const isEmptyValidator = value => {
+export const isEmptyValidator = (value) => {
   if (!value)
     return {
       result: false,
-      error: "Required"
+      error: "Please fill in these details to continue",
     };
 
   return { result: true };
 };
 
-export const isPhoneNumber = value => {
+export const isPhoneNumber = (value) => {
   let error = "";
   const numberRegex = /^\d+$/;
 
-  if (!value) error = "Required";
+  if (!value) error = "Please fill in these details to continue";
   else if (value.length !== 10 || !numberRegex.test(value))
     error = "Please enter a valid phone number";
 
   return error
     ? {
         result: false,
-        error
+        error,
       }
     : { result: true };
 };
