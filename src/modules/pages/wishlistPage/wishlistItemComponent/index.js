4df34aacebdd527244f8f0135bb33e16a8aa09ee
Kunal Pardeshi <kunal.pardeshi@harbingergroup.com>
2020-05-12 10:34:33 +0530

    move to bag added wishlist

--- src/modules/pages/wishlistPage/wishlistItemComponent/index.js
+++ src/modules/pages/wishlistPage/wishlistItemComponent/index.js
@@ -1,19 +1,21 @@
-import React, { Component } from 'react';
-import DivRow from 'CommonComponents/divRow';
-import DivColumn from 'CommonComponents/divColumn';
-import styles from './wishlist_item_component.module.scss';
-import closeIcon from 'Icons/close-icon-black.svg';
-import { removeFromWishlistAction } from 'Core/modules/wishlist/wishlistActions';
+import React, { Component } from "react";
+import DivRow from "CommonComponents/divRow";
+import DivColumn from "CommonComponents/divColumn";
+import styles from "./wishlist_item_component.module.scss";
+import closeIcon from "Icons/close-icon-black.svg";
+import {
+  removeFromWishlistAction,
+  moveToBagAction,
+} from "Core/modules/wishlist/wishlistActions";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
-import { showSuccessFlashMessage } from 'Redux/actions/flashMessageActions';
+import { showSuccessFlashMessage } from "Redux/actions/flashMessageActions";
 import navigatorHoc from "Hoc/navigatorHoc";
-import translatorHoc from 'Hoc/translatorHoc';
+import translatorHoc from "Hoc/translatorHoc";
 class WishlistItemComponent extends Component {
-
   state = {
-    isRemoving: false
-  }
+    isRemoving: false,
+  };
 
   onClickRemove = () => {
     const {
@@ -22,32 +24,52 @@ class WishlistItemComponent extends Component {
       showSuccessFlashMessage,
       translate,
     } = this.props;
-    
+
     this.setState({ isRemoving: true });
     removeFromWishlistAction({
       product_id: wishlistItem.product.id,
-      exhibition_id: wishlistItem.exhibition.id
-    }).then(({payload}) => {
-      if(payload.code == 200 || payload.code == 201) {
-        showSuccessFlashMessage(translate('common.removed_from_wishlist'))
+      exhibition_id: wishlistItem.exhibition.id,
+    })
+      .then(({ payload }) => {
+        if (payload.code == 200 || payload.code == 201) {
+          showSuccessFlashMessage(translate("common.removed_from_wishlist"));
+        }
+        this.setState({ isRemoving: false });
+      })
+      .catch((error) => {
+        this.setState({ isRemoving: false });
+      });
+  };
+
+  handleMoveToBag = (id) => {
+    const { showSuccessFlashMessage, moveToBagAction, translate } = this.props;
+
+    moveToBagAction(id).then(({ payload }) => {
+      if (payload.code === 200 || payload.code === 201) {
+        showSuccessFlashMessage(translate("common.added_to_bag"));
       }
-      this.setState({ isRemoving: false });
-    }).catch(error => {
-      this.setState({ isRemoving: false });
     });
-  }
+  };
 
   render() {
-    const { wishlistItem: { product, exhibition }, translate } = this.props;
+    const {
+      wishlistItem,
+      wishlistItem: { product, exhibition },
+      translate,
+    } = this.props;
     const { isRemoving } = this.state;
 
     return (
-      <DivColumn className={`${styles.wishlist_item_container} ${isRemoving ? styles.is_disabled: null}`}>
+      <DivColumn
+        className={`${styles.wishlist_item_container} ${
+          isRemoving ? styles.is_disabled : null
+        }`}
+      >
         <DivColumn className={styles.wishlist_details_container}>
           <img
             src={closeIcon}
             className={styles.close_icon}
-            onClick={!isRemoving ? this.onClickRemove: null}
+            onClick={!isRemoving ? this.onClickRemove : null}
           />
           <div className={styles.name}>{product.name}</div>
           <div className={styles.description}>{product.short_description}</div>
@@ -55,26 +77,42 @@ class WishlistItemComponent extends Component {
         </DivColumn>
 
         <DivColumn className={styles.additional_details_container}>
-          <div className={styles.title}>{translate('wishlist_page.exhibition')}</div>
+          <div className={styles.title}>
+            {translate("wishlist_page.exhibition")}
+          </div>
           <div className={styles.name}>{exhibition.title}</div>
-          <div className={styles.time}>Expires in:  40 Mins</div>
+          <div className={styles.time}>Expires in: 40 Mins</div>
         </DivColumn>
 
-        <div className={styles.bag_button}>
-          {translate('wishlist_page.move_bag')}
+        <div
+          className={styles.bag_button}
+          onClick={(e) => {
+            e.stopPropagation();
+            this.handleMoveToBag(wishlistItem.id);
+          }}
+        >
+          {translate("wishlist_page.move_bag")}
         </div>
-
       </DivColumn>
     );
   }
 }
 
-
-const mapDispathToProps = dispatch => {
+const mapDispathToProps = (dispatch) => {
   return {
-    removeFromWishlistAction: bindActionCreators(removeFromWishlistAction, dispatch),
-    showSuccessFlashMessage: bindActionCreators(showSuccessFlashMessage, dispatch),
+    moveToBagAction: bindActionCreators(moveToBagAction, dispatch),
+    removeFromWishlistAction: bindActionCreators(
+      removeFromWishlistAction,
+      dispatch
+    ),
+    showSuccessFlashMessage: bindActionCreators(
+      showSuccessFlashMessage,
+      dispatch
+    ),
   };
 };
 
-export default connect(null, mapDispathToProps)(translatorHoc(navigatorHoc(WishlistItemComponent)));
+export default connect(
+  null,
+  mapDispathToProps
+)(translatorHoc(navigatorHoc(WishlistItemComponent)));
