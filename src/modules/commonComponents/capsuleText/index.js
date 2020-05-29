b2db64481fe2347920258a2046860fe94e3ede33
Om Komawar <omkomawar222@gmail.com>
2020-05-29 14:38:14 +0530

    capsule text updated

--- src/modules/commonComponents/capsuleText/index.js
+++ src/modules/commonComponents/capsuleText/index.js
@@ -1,13 +1,16 @@
-import React, { Component } from 'react';
-import styles from './capsule_text.module.scss';
+import React, { Component } from "react";
+import styles from "./capsule_text.module.scss";
 
 export default class CapsuleText extends Component {
   render() {
     const { text, noMargin, className } = this.props;
-    const capsuleStyle = noMargin ? {margin: 0} : {};
+    const capsuleStyle = noMargin ? { margin: 0 } : {};
 
-     return (
-      <div className={`${styles.capsule_items} ${className}`} style={capsuleStyle}>{`#${text}`}</div>
-     )
+    return (
+      <div
+        className={`${styles.capsule_items} ${className}`}
+        style={capsuleStyle}
+      >{`${text}`}</div>
+    );
   }
 }
