ece6b2aa62d459332f4100d021548c41766860fb
Om Komawar <omkomawar222@gmail.com>
2020-07-23 13:39:58 +0530

    fixed payment failure page with translation

--- src/modules/pages/paymentFailurePage/index.js
+++ src/modules/pages/paymentFailurePage/index.js
@@ -5,10 +5,17 @@ import styles from "./payment_failure.module.scss";
 import CapsuleButton from "CommonComponents/capsuleButton";
 import navigatorHoc from "Hoc/navigatorHoc";
 // import queryString from "query-string";
+import translatorHoc from "Hoc/translatorHoc";
+import CloseIcon from "Icons/close-icon.svg";
 
 class PaymentFailurePage extends Component {
+  onClickActionButton = () => {
+    const { navigateTo } = this.props;
+    navigateTo("select-payment");
+  };
   render() {
     // const parsed = queryString.parse(this.props.location.search);
+    const { translate } = this.props;
 
     return (
       <FullWidthContainer>
@@ -17,15 +24,24 @@ class PaymentFailurePage extends Component {
           horizontalCenter
           className={styles.page_container}
         >
-          <div className={styles.title}>Payment Failed</div>
-          <div className={styles.description}>
-            There was problem while accepting your payment.
+          <div className={styles.inner_content_container}>
+            <div className={styles.circle}>
+              <img alt="Failed" className={styles.image} src={CloseIcon} />
+            </div>
+            <div className={styles.title}>
+              {translate("payment_failure_page.title")}
+            </div>
+            <div className={styles.description}>
+              {translate("payment_failure_page.description")}
+            </div>
+            <CapsuleButton onClick={this.onClickActionButton}>
+              {translate("payment_failure_page.action_text")}
+            </CapsuleButton>
           </div>
-          <CapsuleButton>Retry</CapsuleButton>
         </DivColumn>
       </FullWidthContainer>
     );
   }
 }
 
-export default navigatorHoc(PaymentFailurePage);
+export default navigatorHoc(translatorHoc(PaymentFailurePage));
